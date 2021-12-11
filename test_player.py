56b5449f8d06c2d3885c910c5a63b16def8d4d11
Ankush <ankushmark@gmail.com>
2021-12-11 23:04:07 +0000

    unittests

--- test_player.py
+++ test_player.py
@@ -10,15 +10,29 @@ class Test_player(unittest.TestCase):
     
 class TestInit(Test_player):
   def test_initial_score(self):
-    self.assertEqual(self.player.score, 0) 
+    self.assertEqual(self.player.score, 0)
+
   def test_initial_stage(self):
     self.assertEqual(self.player.stage, 1) 
+
   def test_initial_username(self): #username exists
     db = shelve.open('playerdb')
-    self.assertEqual(self.player.username in db, "test")
+    if self.player.username in db:
+      self.assertEqual(self.player.username, "test")
+      db.close()
+    
   def test_initial_name(self): #name exists
-    self.assertEqual(self.player.name, "test")
-  db.close()
+    db = shelve.open('playerdb')
+    if self.player.name == "test" in db:
+      self.assertEqual(self.player.name, "test")
+    db.close()
+  
+  def test_highscore(self):
+    db = shelve.open('playerdb')
+    if self.player.username == "test" in db:
+      self.assertEqual(self.player.highscore, 3)
+      print (self.player.highscore)
+    db.close()
 
 if __name__ == '__main__':
     unittest.main()
