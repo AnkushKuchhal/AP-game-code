b53217d949e31e1554781e8bce66d5d765a90dd2
AnkushKuchhal <94969110+AnkushKuchhal@users.noreply.github.com>
2021-12-12 20:01:10 +0000

    Update test_player.py

--- test_player.py
+++ test_player.py
@@ -6,34 +6,43 @@ from player import Player
 
 class Test_player(unittest.TestCase):
   def setUp(self):
-    self.player = Player()
+    self.player = Player(3, "test", "test", "test")
+    self.player.score = 100
+    self.player.stage = 1
     
 class TestInit(Test_player):
   def test_initial_score(self):
-    self.assertEqual(self.player.score, 0)
+    self.assertEqual(self.player.score, 100)
 
   def test_initial_stage(self):
     self.assertEqual(self.player.stage, 1) 
 
   def test_initial_username(self): #username exists
     db = shelve.open('playerdb')
-    if self.player.username in db:
-      self.assertEqual(self.player.username, "test")
-      db.close()
+    #if self.player.username in db:
+    self.assertEqual(self.player.username, "test")
+    db.close()
     
   def test_initial_name(self): #name exists
     db = shelve.open('playerdb')
-    if self.player.name == "test" in db:
-      self.assertEqual(self.player.name, "test")
+    self.assertEqual(self.player.name, "test")
     db.close()
   
   def test_highscore(self):
     db = shelve.open('playerdb')
-    if self.player.username == "test" in db:
+    if self.player.username in db:
       self.assertEqual(self.player.highscore, 3)
-      print (self.player.highscore)
+    db.close()
+
+
+  def test_auth(self):
+    db = shelve.open('playerdb')
+    if self.player.username in db:
+      password = db[self.player.username].password #type: ignore
+      self.player.hashPassword()
+      self.assertEqual(self.player.authenticatePlayer(), True)
+    else: raise ValueError('Username not found')
     db.close()
 
 if __name__ == '__main__':
     unittest.main()
-   
